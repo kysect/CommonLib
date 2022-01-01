16ff723b9087ab80a4b9cf0df023a9a71d2a5449
Fredi Kats <fredikats@outlook.com>
2022-01-01 20:23:08 +0300

    feat: add work with paths

---
+++ Kysect.CommonLib/Paths/PartialFilePath.cs
@@ -0,0 +1,38 @@
+﻿namespace Kysect.CommonLib.Paths;
+
+public class PartialFilePath
+{
+    public FileInfo File { get; }
+    public PartialPath FilePath { get; }
+    public PartialPath ParentDirectoryPath { get; }
+
+    public PartialFilePath(string root, string fileFullPath)
+    {
+        File = new FileInfo(fileFullPath);
+        FilePath = new PartialPath(root, fileFullPath);
+        ParentDirectoryPath = File.DirectoryName is null
+            ? new PartialPath(string.Empty, string.Empty)
+            : new PartialPath(root, File.DirectoryName);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is PartialFilePath other
+            && Equals(other);
+    }
+
+    public bool Equals(PartialFilePath other)
+    {
+        return FilePath.Equals(other.FilePath);
+    }
+
+    public override int GetHashCode()
+    {
+        return FilePath.GetHashCode();
+    }
+
+    public override string ToString()
+    {
+        return FilePath.ToString();
+    }
+}
---
+++ Kysect.CommonLib/Paths/PartialPath.cs
@@ -0,0 +1,51 @@
+﻿namespace Kysect.CommonLib.Paths;
+
+public class PartialPath
+{
+    public string Root { get; }
+    public string Path { get; }
+
+    public PartialPath(string root, string fullPath)
+    {
+        ArgumentNullException.ThrowIfNull(root);
+        ArgumentNullException.ThrowIfNull(fullPath);
+
+        if (fullPath.StartsWith(root))
+            throw new ArgumentException("Full path should start with root path");
+
+        Root = root;
+
+        if (root == fullPath)
+            Path = string.Empty;
+        else if (string.IsNullOrWhiteSpace(root))
+            Path = fullPath;
+        else
+        {
+            Path = fullPath.Substring(root.Length);
+            if (Path[0] == System.IO.Path.DirectorySeparatorChar)
+                Path = Path.Remove(0, 1);
+        }
+    }
+
+    public static PartialPath FromRoot(string fullPath) => new PartialPath(string.Empty, fullPath);
+
+    public override bool Equals(object? obj)
+    {
+        return obj is PartialPath other && Equals(other);
+    }
+
+    public bool Equals(PartialPath other)
+    {
+        return Path.Equals(other.Path);
+    }
+
+    public override int GetHashCode()
+    {
+        return Path.GetHashCode();
+    }
+
+    public override string ToString()
+    {
+        return Path;
+    }
+}
