6427306d29430f9f6a41e3a3a48e4b205d5af744
Fredi Kats <fredikats@outlook.com>
2022-01-01 20:25:46 +0300

    feat: add reasonable

---
+++ Kysect.CommonLib/Reasonables/Reasonable.cs
@@ -0,0 +1,40 @@
+﻿namespace Kysect.CommonLib.Reasonables
+{
+    public static class Reasonable
+    {
+        public static Reasonable<T> Create<T>(T value) => new Reasonable<T>(value, null);
+        public static Reasonable<T> Create<T>(T value, string reason) => new Reasonable<T>(value, reason);
+    }
+
+    public class Reasonable<T>
+    {
+        public T Value { get; }
+        public string? Reason { get; }
+
+        public Reasonable(T value, string? reason)
+        {
+            ArgumentNullException.ThrowIfNull(value);
+
+            Value = value;
+            Reason = reason;
+        }
+
+        public static implicit operator T(Reasonable<T> value)
+        {
+            return value.Value;
+        }
+
+        public string Format()
+        {
+            if (string.IsNullOrWhiteSpace(Reason))
+                return Value.ToString();
+
+            return $"{Value} ({Reason})";
+        }
+
+        public override string ToString()
+        {
+            return Format();
+        }
+    }
+}
