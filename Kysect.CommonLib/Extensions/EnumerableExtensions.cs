386dc3a9b51c361f0630e216e6af9454e8bc34d9
Fredi Kats <fredikats@outlook.com>
2022-02-19 20:11:08 +0300

    feat: add ToSingleString

---
+++ Kysect.CommonLib/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace Kysect.CommonLib;
+
+public static class EnumerableExtensions
+{
+    public static string ToSingleString<T>(this IEnumerable<T> elements, Func<T, string> selector, string delimiter = ", ")
+    {
+        return elements
+            .Select(selector)
+            .ToSingleString(delimiter);
+    }
+
+    public static string ToSingleString<T>(this IEnumerable<T> elements, string delimiter = ", ")
+    {
+        return string.Join(delimiter, elements);
+    }
+}
