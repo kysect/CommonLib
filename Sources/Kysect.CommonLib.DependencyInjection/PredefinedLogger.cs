d8a7b802d0f0bee9d20180e727855c38ce2733e8
Fredi Kats <fredikats@outlook.com>
2023-09-08 12:10:00 +0400

    Add CommonLib.DependencyInection nuget

---
+++ Sources/Kysect.CommonLib.DependencyInjection/PredefinedLogger.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Kysect.CommonLib.DependencyInjection;
+
+public static class PredefinedLogger
+{
+    public static ILogger CreateConsoleLogger(string category = "ConsoleLogger")
+    {
+        ILoggerFactory factory = LoggerFactory
+            .Create(b =>
+            {
+                b.AddSimpleConsole(options =>
+                {
+                    options.IncludeScopes = true;
+                    options.SingleLine = true;
+                    options.TimestampFormat = "HH:mm:ss ";
+                });
+            });
+
+        return factory.CreateLogger(category);
+    }
+}
