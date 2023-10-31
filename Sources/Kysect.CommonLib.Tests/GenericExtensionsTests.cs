8fda99bda0b2c94a6b69190e657c3be06cb44c8e
Fredi Kats <fredikats@outlook.com>
2023-10-31 19:32:36 +0100

    Add CallerArgumentExpression attribute

---
+++ Sources/Kysect.CommonLib.Tests/GenericExtensionsTests.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using Kysect.CommonLib.BaseTypes.Extensions;
+
+namespace Kysect.CommonLib.Tests;
+
+public class GenericExtensionsTests
+{
+    [Test]
+    public void ThrowIfNull_ShouldThrowExceptionWithCorrectMessage()
+    {
+        int? myVariableName = null;
+
+        ArgumentNullException? argumentNullException = Assert.Throws<ArgumentNullException>(() =>
+        {
+            myVariableName.ThrowIfNull();
+        });
+
+        argumentNullException.Should().NotBeNull();
+        argumentNullException.Message.Should().Be($"Value cannot be null. (Parameter '{nameof(myVariableName)}')");
+    }
+}
---
+++ Sources/Kysect.CommonLib/Internal/CallerArgumentExpressionAttribute.cs
@@ -0,0 +1,15 @@
+﻿#pragma warning disable IDE0130
+// ReSharper disable once CheckNamespace
+namespace System.Runtime.CompilerServices;
+#pragma warning restore IDE0130
+
+[AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false, Inherited = false)]
+internal sealed class CallerArgumentExpressionAttribute : Attribute
+{
+    public CallerArgumentExpressionAttribute(string parameterName)
+    {
+        ParameterName = parameterName;
+    }
+
+    public string ParameterName { get; }
+}
