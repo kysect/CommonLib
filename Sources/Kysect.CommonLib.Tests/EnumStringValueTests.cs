99c12d89b0f0dce8f6207984e69083c128933656
Fredi Kats <fredikats@outlook.com>
2023-11-04 11:07:51 +0100

    Implement attribute for custom ToString for enum values

---
+++ Sources/Kysect.CommonLib.Tests/EnumStringValueTests.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using Kysect.CommonLib.BaseTypes;
+
+namespace Kysect.CommonLib.Tests;
+
+public class EnumStringValueTests
+{
+    public enum TestType
+    {
+        [EnumStringValue("First value")]
+        FirstValue,
+
+        [EnumStringValue("2 value")]
+        SecondValue,
+    }
+
+    [Test]
+    [TestCase(TestType.FirstValue, "First value")]
+    [TestCase(TestType.SecondValue, "2 value")]
+    public void ToEnumStringAndBack_ShouldReturnCorrectValue(TestType enumValue, string stringValue)
+    {
+        string actualStringValue = EnumStringValue.ToEnumString(enumValue);
+        TestType actualEnumValue = EnumStringValue.FromEnumString<TestType>(stringValue);
+
+        actualStringValue.Should().Be(stringValue);
+        actualEnumValue.Should().Be(enumValue);
+    }
+}
---
+++ Sources/Kysect.CommonLib/BaseTypes/EnumStringValueAttribute.cs
@@ -0,0 +1,12 @@
+﻿namespace Kysect.CommonLib.BaseTypes;
+
+[AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = false)]
+public sealed class EnumStringValueAttribute : Attribute
+{
+    public string StringValue { get; }
+
+    public EnumStringValueAttribute(string stringValue)
+    {
+        StringValue = stringValue;
+    }
+}
