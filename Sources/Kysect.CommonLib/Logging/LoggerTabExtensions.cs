16826948bb582e2757644509abf76416418719bd
Fredi Kats <fredikats@outlook.com>
2023-11-05 11:38:23 +0100

    Add more logger tab extensions

--- Sources/Kysect.CommonLib/Logging/LoggerTabExtensions.cs
+++ Sources/Kysect.CommonLib/Logging/LoggerTabExtensions.cs
@@ -4,22 +4,40 @@ namespace Kysect.CommonLib.Logging;
 
 public static class LoggerTabExtensions
 {
-    public static void LogTabError(this ILogger logger, int tabCount, string text)
+    public static void LogTab(this ILogger logger, LogLevel logLevel, int tabCount, string text)
     {
         string prefix = GenerateStringWithTab(tabCount);
-        logger.LogError("{prefix}{message}", prefix, text);
+        logger.Log(logLevel, "{prefix}{message}", prefix, text);
     }
 
-    public static void LogTabInformation(this ILogger logger, int tabCount, string text)
+    public static void LogTabTrace(this ILogger logger, int tabCount, string text)
     {
-        string prefix = GenerateStringWithTab(tabCount);
-        logger.LogInformation("{prefix}{message}", prefix, text);
+        logger.LogTab(LogLevel.Trace, tabCount, text);
     }
 
     public static void LogTabDebug(this ILogger logger, int tabCount, string text)
     {
-        string prefix = GenerateStringWithTab(tabCount);
-        logger.LogDebug("{prefix}{message}", prefix, text);
+        logger.LogTab(LogLevel.Debug, tabCount, text);
+    }
+
+    public static void LogTabInformation(this ILogger logger, int tabCount, string text)
+    {
+        logger.LogTab(LogLevel.Information, tabCount, text);
+    }
+
+    public static void LogTabWarning(this ILogger logger, int tabCount, string text)
+    {
+        logger.LogTab(LogLevel.Warning, tabCount, text);
+    }
+
+    public static void LogTabError(this ILogger logger, int tabCount, string text)
+    {
+        logger.LogTab(LogLevel.Error, tabCount, text);
+    }
+
+    public static void LogTabCritical(this ILogger logger, int tabCount, string text)
+    {
+        logger.LogTab(LogLevel.Critical, tabCount, text);
     }
 
     private static string GenerateStringWithTab(int tabCount)
