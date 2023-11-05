ddd9c5a34bc46d222f4c36e27a31be1bb863cb68
Fredi Kats <fredikats@outlook.com>
2023-11-05 11:13:37 +0100

    Add logging implementation for progress tracking

---
+++ Sources/Kysect.CommonLib/ProgressTracking/LoggerProgressTrackerFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Kysect.CommonLib.ProgressTracking;
+
+public class LoggerProgressTrackerFactory : IProgressTrackerFactory
+{
+    private readonly ILogger _logger;
+    private readonly LogLevel _logLevel;
+
+    public LoggerProgressTrackerFactory(ILogger logger, LogLevel logLevel)
+    {
+        _logger = logger;
+        _logLevel = logLevel;
+    }
+
+    public IProgressTracker Create(string operationName, int maxValue)
+    {
+        return new LoggerProgressTracker(operationName, maxValue, _logger, _logLevel);
+    }
+}
