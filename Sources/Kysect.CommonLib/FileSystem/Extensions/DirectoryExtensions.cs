7f73ef0697c5d8f5f1a2a31b9c1e6c60914c497e
Fredi Kats <fredikats@outlook.com>
2023-12-27 00:08:58 +0100

    Add method for checking parent directory existing with TestableIO

--- Sources/Kysect.CommonLib/FileSystem/Extensions/DirectoryExtensions.cs
+++ Sources/Kysect.CommonLib/FileSystem/Extensions/DirectoryExtensions.cs
@@ -1,13 +1,22 @@
-﻿namespace Kysect.CommonLib.FileSystem.Extensions;
+﻿using Kysect.CommonLib.BaseTypes.Extensions;
+using System.IO.Abstractions;
+
+namespace Kysect.CommonLib.FileSystem.Extensions;
 
 public static class DirectoryExtensions
 {
-    public static void EnsureFileExists(PartialPath path)
+    public static void EnsureParentDirectoryExists(string filePath)
     {
-        EnsureFileExists(path.FullPath);
+        filePath.ThrowIfNull();
+
+        var fileInfo = new FileInfo(filePath);
+        if (fileInfo.Directory is null)
+            throw new ArgumentException($"File {fileInfo.FullName} does not have directory");
+
+        EnsureDirectoryExists(fileInfo.Directory.FullName);
     }
 
-    public static void EnsureFileExists(string path)
+    public static void EnsureDirectoryExists(string path)
     {
         if (path is null)
             throw new ArgumentNullException(nameof(path));
@@ -16,5 +25,36 @@ public static class DirectoryExtensions
             return;
 
         Directory.CreateDirectory(path);
+    }
+
+    public static void EnsureParentDirectoryExists(IFileSystem fileSystem, string path)
+    {
+        fileSystem.ThrowIfNull();
+        path.ThrowIfNull();
+
+        IFileInfo fileInfo = fileSystem.FileInfo.New(path);
+        EnsureParentDirectoryExists(fileSystem, fileInfo);
+    }
+
+    public static void EnsureParentDirectoryExists(IFileSystem fileSystem, IFileInfo file)
+    {
+        fileSystem.ThrowIfNull();
+        file.ThrowIfNull();
+
+        if (file.Directory is null)
+            throw new ArgumentException($"File {file.FullName} does not have directory");
+
+        EnsureDirectoryExists(fileSystem, file.Directory);
+    }
+
+    public static void EnsureDirectoryExists(IFileSystem fileSystem, IDirectoryInfo directory)
+    {
+        fileSystem.ThrowIfNull();
+        directory.ThrowIfNull();
+
+        if (fileSystem.Directory.Exists(directory.FullName))
+            return;
+
+        fileSystem.Directory.CreateDirectory(directory.FullName);
     }
 }
