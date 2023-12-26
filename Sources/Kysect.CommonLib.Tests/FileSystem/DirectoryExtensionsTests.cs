b147424fef7aa071c3c270815c2ecc586176882d
Fredi Kats <fredikats@outlook.com>
2023-12-27 00:08:58 +0100

    Add method for checking parent directory existing with TestableIO

---
+++ Sources/Kysect.CommonLib.Tests/FileSystem/DirectoryExtensionsTests.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using Kysect.CommonLib.FileSystem.Extensions;
+using System.IO.Abstractions.TestingHelpers;
+
+namespace Kysect.CommonLib.Tests.FileSystem;
+
+public class DirectoryExtensionsTests
+{
+    private MockFileSystem _fileSystem;
+
+    [SetUp]
+    public void Setup()
+    {
+        _fileSystem = new MockFileSystem();
+    }
+
+    [Test]
+    public void EnsureParentDirectoryExists_ForNonExistingDirectory_CreateDirectory()
+    {
+        const string directoryPath = "Dir";
+        string filePath = _fileSystem.Path.Combine(directoryPath, "File.txt");
+
+        _fileSystem.Directory.Exists(directoryPath).Should().BeFalse();
+        DirectoryExtensions.EnsureParentDirectoryExists(_fileSystem, filePath);
+        _fileSystem.Directory.Exists(directoryPath).Should().BeTrue();
+    }
+}
