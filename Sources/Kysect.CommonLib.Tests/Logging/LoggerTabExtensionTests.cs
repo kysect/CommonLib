7789955a4845441f45f49bc5f02e3f996c04b7ce
Fredi Kats <fredikats@outlook.com>
2024-01-01 00:19:25 +0100

    Add tests for logger tab extensions

---
+++ Sources/Kysect.CommonLib.Tests/Logging/LoggerTabExtensionTests.cs
@@ -0,0 +1,64 @@
+﻿using FluentAssertions;
+using Kysect.CommonLib.Logging;
+using Kysect.CommonLib.Tests.Logging.Fakes;
+
+namespace Kysect.CommonLib.Tests.Logging;
+
+public class LoggerTabExtensionTests
+{
+    private StringBuilderLogger _logger;
+
+    [SetUp]
+    public void Setup()
+    {
+        _logger = new StringBuilderLogger();
+    }
+
+    [Test]
+    public void LogTabTrace_ReturnExpectedString()
+    {
+        _logger.LogTabTrace(1, "Message");
+
+        _logger.Build().Should().BeEquivalentTo(["\tMessage"]);
+    }
+
+    [Test]
+    public void LogTabDebug_ReturnExpectedString()
+    {
+        _logger.LogTabDebug(1, "Message");
+
+        _logger.Build().Should().BeEquivalentTo(["\tMessage"]);
+    }
+
+    [Test]
+    public void LogTabInformation_ReturnExpectedString()
+    {
+        _logger.LogTabInformation(1, "Message");
+
+        _logger.Build().Should().BeEquivalentTo(["\tMessage"]);
+    }
+
+    [Test]
+    public void LogTabWarning_ReturnExpectedString()
+    {
+        _logger.LogTabWarning(1, "Message");
+
+        _logger.Build().Should().BeEquivalentTo(["\tMessage"]);
+    }
+
+    [Test]
+    public void LogTabError_ReturnExpectedString()
+    {
+        _logger.LogTabError(1, "Message");
+
+        _logger.Build().Should().BeEquivalentTo(["\tMessage"]);
+    }
+
+    [Test]
+    public void LogTabCritical_ReturnExpectedString()
+    {
+        _logger.LogTabCritical(1, "Message");
+
+        _logger.Build().Should().BeEquivalentTo(["\tMessage"]);
+    }
+}
