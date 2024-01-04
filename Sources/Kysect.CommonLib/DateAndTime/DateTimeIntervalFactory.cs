db2d28e4689cc2a71c94166281d69986fd83877b
Fredi Kats <fredikats@outlook.com>
2024-01-04 11:59:06 +0100

    Add some basic time abstractions

---
+++ Sources/Kysect.CommonLib/DateAndTime/DateTimeIntervalFactory.cs
@@ -0,0 +1,41 @@
+﻿namespace Kysect.CommonLib.DateAndTime;
+
+public class DateTimeIntervalFactory
+{
+    public IReadOnlyCollection<DateTimeInterval> Create(DateTimeOffset start, DateTimeOffset end, DayOfWeekTime dayOfWeekTime)
+    {
+        if (start > end)
+            throw new ArgumentException("Interval end is lower that start.");
+
+        DateTimeOffset currentStart = dayOfWeekTime.GetNextPointInTime(start);
+        var result = new List<DateTimeInterval>();
+
+        if (currentStart > end)
+        {
+            result.Add(new DateTimeInterval(start, end));
+            return result;
+        }
+
+        if (start < currentStart)
+        {
+            result.Add(new DateTimeInterval(start, currentStart));
+        }
+        else
+        {
+            currentStart = start;
+        }
+
+        while (currentStart.AddDays(7) < end)
+        {
+            DateTimeOffset intervalEnd = currentStart.AddDays(7);
+            result.Add(new DateTimeInterval(currentStart, intervalEnd));
+
+            currentStart = intervalEnd;
+        }
+
+        if (currentStart != end)
+            result.Add(new DateTimeInterval(currentStart, end));
+
+        return result;
+    }
+}
---
+++ Sources/Kysect.CommonLib/DateAndTime/DayOfWeekTime.cs
@@ -0,0 +1,17 @@
+﻿namespace Kysect.CommonLib.DateAndTime;
+
+public record struct DayOfWeekTime(DayOfWeek DayOfWeek, int Hours, int Minutes)
+{
+    public readonly DateTimeOffset GetNextPointInTime(DateTimeOffset from)
+    {
+        if (from.DayOfWeek == DayOfWeek)
+        {
+            if (from.Hour < Hours || (from.Hour == Hours && from.Minute <= Minutes))
+                return from.Date.AddHours(Hours).AddMinutes(Minutes);
+        }
+
+        SelectedDayOfWeek selectedDayOfWeek = SelectedDayOfWeekExtensions.ConvertToSelectedDayOfWeek(DayOfWeek);
+        DateTimeOffset dateTimeLocal = DateFinder.GetNearestNext(from, selectedDayOfWeek);
+        return dateTimeLocal.Date.AddHours(Hours).AddMinutes(Minutes);
+    }
+}
