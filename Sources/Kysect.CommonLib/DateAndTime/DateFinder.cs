a5adbfe636b12b654ff8f09c0d6263147bc968ee
Fredi Kats <fredikats@outlook.com>
2024-01-04 11:59:06 +0100

    Add some basic time abstractions

---
+++ Sources/Kysect.CommonLib/DateAndTime/DateFinder.cs
@@ -0,0 +1,46 @@
+﻿namespace Kysect.CommonLib.DateAndTime;
+
+public static class DateFinder
+{
+    public static DateTimeOffset GetNearestPrevious(DateTimeOffset currentDate, SelectedDayOfWeek mask)
+    {
+        currentDate = currentDate.Date;
+        if (mask == SelectedDayOfWeek.None)
+            throw new ArgumentException("Cannot find date by empty mask");
+
+        while (!SelectedDayOfWeekExtensions.Contains(mask, currentDate.DayOfWeek))
+        {
+            currentDate = currentDate.AddDays(-1);
+        }
+
+        return currentDate;
+    }
+
+    public static DateTimeOffset GetNearestNext(DateTimeOffset currentDate, SelectedDayOfWeek mask)
+    {
+        currentDate = currentDate.Date;
+        if (mask == SelectedDayOfWeek.None)
+            throw new ArgumentException("Cannot find date by empty mask");
+
+        while (!SelectedDayOfWeekExtensions.Contains(mask, currentDate.DayOfWeek))
+        {
+            currentDate = currentDate.AddDays(1);
+        }
+
+        return currentDate;
+    }
+
+    public static DateTimeOffset GetPrevious(DateTimeOffset currentDate, SelectedDayOfWeek mask)
+    {
+        currentDate = currentDate.Date;
+        if (mask == SelectedDayOfWeek.None)
+            throw new ArgumentException("Cannot find date by empty mask");
+
+        do
+        {
+            currentDate = currentDate.AddDays(-1);
+        } while (!SelectedDayOfWeekExtensions.Contains(mask, currentDate.DayOfWeek));
+
+        return currentDate;
+    }
+}
---
+++ Sources/Kysect.CommonLib/DateAndTime/DateTimeInterval.cs
@@ -0,0 +1,42 @@
+﻿namespace Kysect.CommonLib.DateAndTime;
+
+public readonly struct DateTimeInterval : IEquatable<DateTimeInterval>
+{
+    public DateTimeOffset Start { get; }
+    public DateTimeOffset End { get; }
+
+    public DateTimeInterval(DateTimeOffset start, DateTimeOffset end)
+    {
+        if (start > end)
+            throw new ArgumentException("Interval end is lower that start.");
+
+        Start = start;
+        End = end;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is DateTimeInterval interval && Equals(interval);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Start, End);
+    }
+
+    public static bool operator ==(DateTimeInterval left, DateTimeInterval right)
+    {
+        return left.Equals(right);
+    }
+
+    public static bool operator !=(DateTimeInterval left, DateTimeInterval right)
+    {
+        return !(left == right);
+    }
+
+    public bool Equals(DateTimeInterval other)
+    {
+        return Start.Equals(other.Start) &&
+               End.Equals(other.End);
+    }
+}
