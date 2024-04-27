f31fc0ae1adb2872bbc42c5deceaaa70e63b349d
Fredi Kats <fredikats@outlook.com>
2024-04-27 12:33:08 +0200

    Add TestTemporaryDirectory

---
+++ Sources/Kysect.CommonLib.Testing/TestLoggerProvider.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Logging;
+
+#if DEBUG
+using Kysect.CommonLib.DependencyInjection.Logging;
+#else
+using Microsoft.Extensions.Logging.Abstractions;
+#endif
+
+namespace Kysect.CommonLib.Testing;
+
+public static class TestLoggerProvider
+{
+    public static ILogger Provide()
+    {
+#if DEBUG
+        return DefaultLoggerConfiguration.CreateConsoleLogger();
+#else
+        return NullLogger.Instance;
+#endif
+    }
+}
---
+++ Sources/Kysect.CommonLib.Testing/TestTemporaryDirectory.cs
@@ -0,0 +1,67 @@
+﻿using Kysect.CommonLib.BaseTypes.Extensions;
+using Kysect.CommonLib.FileSystem;
+using System.IO.Abstractions;
+
+namespace Kysect.CommonLib.Testing;
+
+public class TestTemporaryDirectory : IDisposable
+{
+    private readonly System.IO.Abstractions.FileSystem _fileSystem;
+    private readonly IDirectoryInfo _directoryInfo;
+
+    public TestTemporaryDirectory(System.IO.Abstractions.FileSystem fileSystem, string rootPath = ".")
+    {
+        _fileSystem = fileSystem.ThrowIfNull();
+        rootPath.ThrowIfNull();
+
+        string path = _fileSystem.Path.Combine(rootPath, "TempDirectory", Guid.NewGuid().ToString());
+
+        if (_fileSystem.Directory.Exists(path))
+        {
+            IDirectoryInfo directoryInfo = _fileSystem.DirectoryInfo.New(path);
+            DeleteRecursive(directoryInfo);
+        }
+
+        _directoryInfo = _fileSystem.Directory.CreateDirectory(path);
+        fileSystem.EnsureDirectoryExists(path);
+    }
+
+    public string GetTemporaryDirectory()
+    {
+        return _fileSystem.Path.Combine(_directoryInfo.FullName, Guid.NewGuid().ToString());
+    }
+
+    public void Dispose()
+    {
+        DeleteRecursive(_directoryInfo);
+    }
+
+    // KB: https://github.com/libgit2/libgit2sharp/issues/1354
+    public static void DeleteRecursive(IDirectoryInfo target)
+    {
+        target.ThrowIfNull();
+
+        if (!target.Exists)
+        {
+            return;
+        }
+
+        foreach (var file in target.EnumerateFiles())
+        {
+            if (file.IsReadOnly)
+            {
+                file.IsReadOnly = false;
+            }
+
+            file.Delete();
+        }
+
+        foreach (IDirectoryInfo dir in target.EnumerateDirectories())
+        {
+            DeleteRecursive(dir);
+        }
+
+
+        target.Delete();
+    }
+}
